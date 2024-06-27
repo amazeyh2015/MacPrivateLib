d62101c525e6466f7214197ef0daed65a10c7c63
tengsheng.yh <tengsheng.yh@alibaba-inc.com>
2024-06-27 13:42:56 +0800

    add MobileDevice

---
+++ Sources/MacPrivateLib/MobileDevice.m
@@ -0,0 +1,12 @@
+//
+//  MobileDevice.m
+//  
+//
+//  Created by yuhan on 2024/6/27.
+//
+
+#import "MobileDevice.h"
+
+@implementation MobileDevice
+
+@end
---
+++ Sources/MacPrivateLib/include/MobileDevice.h
@@ -0,0 +1,16 @@
+//
+//  MobileDevice.h
+//  
+//
+//  Created by yuhan on 2024/6/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MobileDevice : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
